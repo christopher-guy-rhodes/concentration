c86d36877590e5064d23b0080d5abb5884ad2893
Christopher Rhodes <crhodes@amazon.com>
2021-11-08 01:05:02 -0800

    Removed IMAGE_OFFSETS constant, using implicit offsets while looping over rank and suit

--- js/deck/PlayingCardDeck.js
+++ js/deck/PlayingCardDeck.js
@@ -1,18 +1,24 @@
 /**
  * Class that models a deck of playing cards.
  */
+// Must match order of suit rows in PLAYING_CARD_IMAGE
 const PLAYING_CARD_DECK_SUITS = [SPADES, HEARTS, DIAMONDS, CLUBS];
-const PLAYING_CARD_RANKS = [TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, K, Q, J, A];
+// MuST match order of rank columns in PLAYING_CARD_IMAGE
+const PLAYING_CARD_RANKS = [A, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, J, Q, K];
 
 let cards = [];
+let i, j = 0;
 for (let suit of PLAYING_CARD_DECK_SUITS) {
     for (let rank of PLAYING_CARD_RANKS) {
         let id = rank + '-' + suit;
-        let x = -1 * IMAGE_OFFSETS[id]['x'] * CARD_WIDTH;
-        let y = -1 * IMAGE_OFFSETS[id]['y'] * CARD_HEIGHT;
-        let card = new PlayingCard(rank, suit, x, y, PLAYING_CARD_IMAGE);
+        let xImageOffset = -1 * i * CARD_WIDTH;
+        let yImageOffset = -1 * j * CARD_HEIGHT;
+        let card = new PlayingCard(rank, suit, xImageOffset, yImageOffset, PLAYING_CARD_IMAGE);
         cards.push(card);
+        i++;
     }
+    i = 0;
+    j++;
 }
 const PLAYING_CARDS = cards;
 
